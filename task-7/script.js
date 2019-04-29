b92585eccb17c5ba939276ed4fd7348a9e80c55c
malinaNata <kalinka007@tut.by>
2019-04-29 19:54:55 +0300

    refactor code

--- task-7/script.js
+++ task-7/script.js
@@ -1,16 +1,16 @@
-let div = document.createElement('div'),
-    p = document.createElement('p');
-
-document.body.appendChild(div);
-p.innerHTML = '00:00:00';
-div.appendChild(p);
-document.body.style.backgroundColor = "pink";
-div.style.textAlign = "center";
-p.style.textAlign = "center";
-p.style.fontSize = "50" + 'px';
-p.style.marginTop = '0';
-
 window.addEventListener('DOMContentLoaded', function () {
+    let div = document.createElement('div'),
+        p = document.createElement('p');
+
+    document.body.appendChild(div);
+    p.innerHTML = '00:00:00';
+    div.appendChild(p);
+    document.body.style.backgroundColor = "pink";
+    div.style.textAlign = "center";
+    p.style.textAlign = "center";
+    p.style.fontSize = "50" + 'px';
+    p.style.marginTop = '0';
+
     // window.timerId = window.setInterval(setTimer, 500);
     let timerId = window.setInterval(function setTimer() {
         let date = new Date();
