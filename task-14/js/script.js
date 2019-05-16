238a92cec1fd2bc59070c0305c0070cb3725ed91
malinaNata <kalinka007@tut.by>
2019-05-17 00:15:33 +0300

    refactor code

--- task-14/js/script.js
+++ task-14/js/script.js
@@ -1,22 +1,12 @@
-$(document).ready(function() {
-
-    function go(sel) {
-        sel.on('click', function() {
-            $('.overlay').fadeTo('slow', 1);
-            $('.modal').slideDown('slow');
-        });
-    }
-
-    go($('.col-sm-3:eq(1)'));
-    go($('.col-sm-3:eq(2)'));
-    go($('[href="#sheldure"]'));
-
-    $('.close').on('click', function() {
-        $('.overlay').fadeOut('slow', 0);
-        $('.modal').slideUp('slow');
+$(window).ready(() => {
+ 
+    $('.col-sm-3:eq(1), .col-sm-3:eq(2), [href="#sheldure"]').on('click', () => {
+        $('.overlay').fadeTo('slow', 1);
+        $('.modal').slideDown('slow');
     });
 
-    
-
-    
+    $('.close').on('click', () => {
+        $('.overlay').fadeOut('slow', 0);
+        $('.modal').slideUp('slow');
+    });    
 });
